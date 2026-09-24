27bf5b67a78ac4e0bc6267cdee13769eddcaac66
emilioger30 <emilio.ger.30@gmail.com>
2026-09-24 11:42:28 -0400

    Add files via upload

---
+++ Programa 2.5/main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <math.h>
+
+/*Funcion matematica.
+El programa obtiene el resultado de una funcion.
+OP y T: variables de tiempo entero.
+RES: variable de tiempo real. */
+
+void main(void)
+{
+int OP, T;
+float RES;
+printf("Ingrese la opcion del calculo y el valor entero: ");
+scanf("%d %d", &OP, &T);
+switch(OP)
+{
+    case 1: RES=T/5;
+      break;
+   case 2: RES=pow(T,T);
+   /*La fuincion pow esta definida en la biblioteca math.h*/
+      break;
+    case 3:
+    case 4: RES=6*T/2;
+      break;
+    default: RES=1;
+      break;
+}
+printf("\nResultado: %7.2",RES);
+}

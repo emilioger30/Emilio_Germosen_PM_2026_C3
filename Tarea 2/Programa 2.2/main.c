3a4668de3780bbec7329e456207a445ee6b18426
emilioger30 <emilio.ger.30@gmail.com>
2026-09-25 09:02:58 -0400

    Add files via upload

---
+++ Tarea 2/Programa 2.2/main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* Incremento de precio
+El programa, al recibir como dato el precio de un producto importado, incrementa 11% el mismo si este es inferior a $1,500.
+PRE y NPR: variable de tipo real. */
+
+void main(void)
+{
+float PRE, NPR;
+printf( "ingrese el precio del producto: ");
+scanf("%f", &PRE);
+if(PRE > 1500)
+{
+    NPR = PRE*1.11;
+    printf("\nNuevo Precio: %7.2f",NPR);
+}
+}
